0193bff5c59ed481c46f3a6a4a9b539b425ac32f
Murli913 <murlitalreja913@gmail.com>
2024-03-14 13:56:09 +0530

    updated lobby features

--- client/src/Screens/Lobby.js
+++ client/src/Screens/Lobby.js
@@ -1,19 +1,30 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useSocket } from '../context/SocketProvider';
 
 const Lobby = () => {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
+  const socket=useSocket();
 
   const handleSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
-  console.log({
-    email,
-    room,
-  });
+ socket.emit("room:join", {email,room})
     },
-    [email, room]
+    [email, room, socket]
   );
+
+  const handleJoinRoom = useCallback((data)=> {
+    const{email,room}=data;
+    console.log(email, room);
+  }, []);
+  useEffect(() => {
+    socket.on("room:join", handleJoinRoom);
+      //console.log(`Data from BE ${data}`);
+    return () => {
+      socket.off('room:join',handleJoinRoom)
+    }
+  }, [socket,handleJoinRoom]);
   return (
     <div>
     <h1>Lobby</h1>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,16 @@ const io = new Server(8000,{
 cors: true,
 });
 
+const emailToSocketIdMap=new Map();
+const socketidToEmailMap=new Map();
   io.on("connection", (socket) => {
+
 console.log(`Socket Connected`, socket.id);
+socket.on("room:join", (data)=>{
+  console.log(data);
+  const { email, room } = data;
+  emailToSocketIdMap.set(email,socket.id);
+  socketidToEmailMap.set(socket.id,email);
+  io.to(socket.id).emit("room:join", data);
+})
     });
